1ced941e233ff128d6d00e6540df03ec7ae79b00
happyhear <r.happyhear@alustudent.com>
2026-06-28 23:53:36 -0700

    python-hello_world: tasks 0-9

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, object-oriented, high-level programming language with dynamic semantics, generally used for general purpose programming. As of 2019, it was one of the most popular language in the world"
+print(str[26:41] + str[6] + str[54:65] +
+      str[6] + str[75:79] + str[6] +
+      str[0:6])
