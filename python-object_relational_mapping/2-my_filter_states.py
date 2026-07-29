7b2cad6fa1b34e1aaae1bead47a75bac56c4d2af
happyhear <r.happyhear@alustudent.com>
2026-07-29 07:53:24 -0700

    add python-object_relational_mapping tasks

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states matching name argument (vulnerable to SQL injection,
+built with format on purpose per task requirements)"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        sys.argv[4])
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
